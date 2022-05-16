7e597d91dfb56aa5e2ae438e18a0c849466ae5ac
indianbleach <alabamableach@gmail.com>
2022-05-16 16:24:39 +0300

    add health checks (MongoDb check)

--- CatalogWebApi.Api/Startup.cs
+++ CatalogWebApi.Api/Startup.cs
@@ -3,6 +3,7 @@ using CatalogWebApi.Infrastructure.Data.Settings;
 using DocumentFormat.OpenXml.EMMA;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -17,6 +18,8 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CatalogWebApi.Api
@@ -33,18 +36,23 @@ namespace CatalogWebApi.Api
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
+            
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings))
+                    .Get<MongoDbSettings>();
 
             services.AddSingleton<IMongoClient>(serviceProvider =>
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings))
-                    .Get<MongoDbSettings>();
+                
 
-                 return new MongoClient(settings.ConnectionString);
+                 return new MongoClient(mongoDbSettings.ConnectionString);
 
             });
 
             services.AddApplicationServices();
+
+            services.AddHealthChecks()
+                .AddMongoDb(mongoDbSettings.ConnectionString, 
+                    name: "MongoDbTimeout", timeout: TimeSpan.FromSeconds(3));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -72,6 +80,26 @@ namespace CatalogWebApi.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapHealthChecks("/health/ready", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions(
+                { 
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async(context, report) => {
+                        var result = JsonSerializer.Serialize(new
+                        {
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(x => new
+                            {
+                                name = x.Key,
+                                status = x.Value.Status.ToString(),
+                                exception = x.Value.Exception != null ? x.Value.Exception.Message : "none",
+                            })
+                        });
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
             });
         }
     }
